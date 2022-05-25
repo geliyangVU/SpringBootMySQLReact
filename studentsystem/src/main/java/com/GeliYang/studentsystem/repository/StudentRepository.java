9e808779c21f9593e8a2724a9b88b52ed97859bc
Geli Yang <gyang5@villanova.edu>
2022-05-25 11:20:02 -0400

    set up package structure
    set up project structure including controller folder, model folder, repository folder and service folder

---
+++ studentsystem/src/main/java/com/GeliYang/studentsystem/repository/StudentRepository.java
@@ -0,0 +1,4 @@
+package com.GeliYang.studentsystem.repository;
+
+public interface StudentRepository {
+}
---
+++ studentsystem/src/main/java/com/GeliYang/studentsystem/service/StudentServiceImpl.java
@@ -0,0 +1,4 @@
+package com.GeliYang.studentsystem.service;
+
+public class StudentServiceImpl {
+}
