caa78ddab6ed05d37e8526daae5b4cc382c53dc0
Geli Yang <gyang5@villanova.edu>
2022-05-26 12:21:09 -0400

    Implemented add student end points

--- studentsystem/src/main/java/com/GeliYang/studentsystem/service/StudentService.java
+++ studentsystem/src/main/java/com/GeliYang/studentsystem/service/StudentService.java
@@ -1,4 +1,8 @@
 package com.GeliYang.studentsystem.service;
 
+import com.GeliYang.studentsystem.model.Student;
+
 public interface StudentService {
+    public Student saveStudent(Student student);
+
 }
--- studentsystem/src/main/java/com/GeliYang/studentsystem/service/StudentServiceImpl.java
+++ studentsystem/src/main/java/com/GeliYang/studentsystem/service/StudentServiceImpl.java
@@ -1,4 +1,19 @@
 package com.GeliYang.studentsystem.service;
 
-public class StudentServiceImpl {
+import com.GeliYang.studentsystem.model.Student;
+import com.GeliYang.studentsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
 }
