ab1e0963d1b66fa86fd8590382e79d3d0388fa0f
Geli Yang <gyang5@villanova.edu>
2022-05-26 12:32:37 -0400

    implemented get mapping endpoint

--- studentsystem/src/main/java/com/GeliYang/studentsystem/controller/StudentController.java
+++ studentsystem/src/main/java/com/GeliYang/studentsystem/controller/StudentController.java
@@ -4,10 +4,9 @@ package com.GeliYang.studentsystem.controller;
 import com.GeliYang.studentsystem.model.Student;
 import com.GeliYang.studentsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -20,5 +19,12 @@ public class StudentController {
     public String add(@RequestBody Student student){
         studentService.saveStudent(student);
         return "New student is added";
+    }
+
+
+
+    @GetMapping("/getAllStudents")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
     }
 }
--- studentsystem/src/main/java/com/GeliYang/studentsystem/service/StudentService.java
+++ studentsystem/src/main/java/com/GeliYang/studentsystem/service/StudentService.java
@@ -2,7 +2,10 @@ package com.GeliYang.studentsystem.service;
 
 import com.GeliYang.studentsystem.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 
 }
